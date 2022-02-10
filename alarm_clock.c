ba4fe881b8e1b41144e508ef2aaa7f8d73c3425d
Oskar Jorgensen <oskarjorgensen@gmail.com>
2022-02-10 10:07:49 +0100

    add function for each choice

--- alarm_clock.c
+++ alarm_clock.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 
+char getChoice() {
+    char choice;
+    printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit): ");
+    scanf("%c", &choice);
+    return choice;
+}
+
+void schedule() {
+    printf("Schedule\n");
+}
+
+void list() {
+    printf("List\n");
+}
+
+void cancel() {
+    printf("Cancel\n");
+}
+
+void exit_program() {
+    printf("Exit\n");
+}
+
 int main() {
-    char a;
-    printf("Type a number: ");
-    scanf("%c", &a);
-    printf("%c", a);
+    char choice;
+    printf("Welcome to the alarm clock!");
+    choice = getChoice();
+    switch (choice)
+    {
+    case 's':
+        schedule();
+        break;
+    case 'l':
+        list();
+        break;
+    case 'c':
+        cancel();
+        break;
+    case 'x':
+        exit_program();
+        break;
+    
+    default:
+        break;
+    }
 }
